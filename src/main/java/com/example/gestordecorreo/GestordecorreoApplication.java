6c431caf154a421c13ffba464f4b49916c5bb0b7
Joaco981 <joaquinnfloress16@gmail.com>
2024-09-25 09:55:06 -0300

    Creación de proyecto
    Co-authored-by: Carl Marturet :P <CarlaMarturet@users.noreply.github.com>

---
+++ src/main/java/com/example/gestordecorreo/GestordecorreoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestordecorreo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestordecorreoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestordecorreoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestordecorreo/GestordecorreoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestordecorreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestordecorreoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
