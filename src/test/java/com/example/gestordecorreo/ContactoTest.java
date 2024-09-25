e69c9acf093d2142615c13364a573f95a76a1578
Joaco981 <joaquinnfloress16@gmail.com>
2024-09-25 10:08:57 -0300

    Creacion de clases principales
    Co-authored-by: Carl Marturet :P <CarlaMarturet@users.noreply.github.com>
    Co-authored-by: Candecano <Candecano@users.noreply.github.com>

--- src/test/java/com/example/gestordecorreo/ContactoTest.java
+++ src/test/java/com/example/gestordecorreo/ContactoTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestordecorreoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+public class ContactoTest {
+    @Test
+    void testCrearContacto() {
+    
+    }
 
 }
---
+++ src/test/java/com/example/gestordecorreo/FiltroTest.java
@@ -0,0 +1,5 @@
+package com.example.gestordecorreo;
+
+public class FiltroTest {
+    
+}
